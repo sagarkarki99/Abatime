d2925babf1600c36bc7ed954a3af363cd455ece7
sagarkarki92 <sagarkarki184@gmail.com>
2021-04-20 21:22:53 +0545

    sign app for release

--- lib/config/utils/ad_manager.dart
+++ lib/config/utils/ad_manager.dart
@@ -4,9 +4,15 @@ class AdManager {
   Future<InitializationStatus> initialization;
   AdManager(this.initialization);
 
-  String get bannerAdUnitId => 'ca-app-pub-3940256099942544/6300978111';
+   String testNativeAdId = 'ca-app-pub-3940256099942544/2247696110';
+  String liveNativeAdId = 'ca-app-pub-4483204795222749/4742061101';
+
+  String liveBannerAdId = 'ca-app-pub-4483204795222749/9057772722';
+  String testBannerAdId = 'ca-app-pub-3940256099942544/6300978111'; 
+
+  String get bannerAdUnitId => liveBannerAdId;
   String get appOpenAdUnitId => 'ca-app-pub-3940256099942544/3419835294';
-  String get nativeAdUnitId => 'ca-app-pub-3940256099942544/2247696110';
+  String get nativeAdUnitId => liveNativeAdId;
   String get nativeVideoAdUnitId => 'ca-app-pub-3940256099942544/1044960115';
 
   AdListener adListener = AdListener();
