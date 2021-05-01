f8859deaaa7479d00db32399104e5e846941c715
sagarkarki92 <sagarkarki184@gmail.com>
2021-05-01 14:44:25 +0545

    change versionCode

--- lib/config/utils/ad_manager.dart
+++ lib/config/utils/ad_manager.dart
@@ -7,13 +7,10 @@ class AdManager {
    String testNativeAdId = 'ca-app-pub-3940256099942544/2247696110';
   String liveNativeAdId = 'ca-app-pub-4483204795222749/4742061101';
 
-  String liveBannerAdId = 'ca-app-pub-4483204795222749/9057772722';
-  String testBannerAdId = 'ca-app-pub-3940256099942544/6300978111'; 
+  String liveBannerAdId = 'ca-app-pub-4483204795222749/5058514040';
 
   String get bannerAdUnitId => liveBannerAdId;
-  String get appOpenAdUnitId => 'ca-app-pub-3940256099942544/3419835294';
   String get nativeAdUnitId => liveNativeAdId;
-  String get nativeVideoAdUnitId => 'ca-app-pub-3940256099942544/1044960115';
 
   AdListener adListener = AdListener();
 }
